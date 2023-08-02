30ab4351b5adf121540afda189550ea8eaa32590
Kennedy Richard Fernandes Garcia <k.richard@academico.ifrn.edu.br>
2023-08-01 22:09:57 -0300

    Repositorio atualizado

--- email/start_email.sh
+++ email/start_email.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-service dovecot start
-/usr/sbin/postfix start-fg
+cd /etc/postfix
+rm main.cf
+
+cd ../dovecot
+rm dovecot.conf
+
+cd conf.dovecot
+rm 10-auth.conf 10-mail.conf 10-master.conf
+
+#service dovecot start
+#/usr/sbin/postfix start-fg
